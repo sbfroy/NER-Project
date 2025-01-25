f1b25773c7d5a68cb80b298babf7ea3c1d1db185
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-01-25 12:55:01 +0100

    initial data loading

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,8 +1,29 @@
-from pypdf import PdfReader
+from conllu import parse
+import pandas as pd
+from pathlib import Path
 
-def get_text(pdf_path):
-    reader = PdfReader(pdf_path)
-    text = ''
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
+base_dir = Path(__file__).parent.parent.parent
+
+def create_df(file_path):
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        sentences = parse(f.read())
+
+    data = {'words': [], 'labels': []}
+
+    for sentence in sentences:
+
+        words = [word['form'] for word in sentence]
+        labels = [word['misc']['name'] for word in sentence]
+
+        data['words'].append(words)
+        data['labels'].append(labels)
+
+    return pd.DataFrame(data)
+
+
+file_path = base_dir /'data/norne/ud/nob/no_bokmaal-ud-train.conllu'
+
+df = create_df(file_path)
+
+print(df.head())
