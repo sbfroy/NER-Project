e30f02fed23d03762414ce5c6748561a3691aced
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-01-25 16:41:03 +0100

    some comments

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,11 +1,17 @@
-from conllu import parse
 import pandas as pd
+from conllu import parse
 from pathlib import Path
 
 base_dir = Path(__file__).parent.parent.parent
 
 def create_df(file_path):
 
+    """
+    Reads a CONLL-U formatted file, extracts words and enitity labels, 
+    and returns a df with the data.
+    """
+
+    # Open and read the CoNLL-U file
     with open(file_path, 'r', encoding='utf-8') as f:
         sentences = parse(f.read())
 
@@ -13,17 +19,12 @@ def create_df(file_path):
 
     for sentence in sentences:
 
-        words = [word['form'] for word in sentence]
-        labels = [word['misc']['name'] for word in sentence]
+        # TODO: Add some error handling if misisng data
+
+        words = [word['form'] for word in sentence] # Extract the words
+        labels = [word['misc']['name'] for word in sentence] # Extract the entity labels
 
         data['words'].append(words)
         data['labels'].append(labels)
 
     return pd.DataFrame(data)
-
-
-file_path = base_dir /'data/norne/ud/nob/no_bokmaal-ud-train.conllu'
-
-df = create_df(file_path)
-
-print(df.head())
