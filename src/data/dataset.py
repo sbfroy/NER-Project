17fc836af3a4d9b665b8ed539d52268432909ae9
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-01-25 21:11:27 +0100

    added the allign_labels function

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,5 +1,6 @@
 import torch
-import torch.utils.data import Dataset
+from torch.utils.data import Dataset
+from src.utils.label_mapping import label_to_id
 
 class Dataset(Dataset):
     def __init__(self, df, tokenizer, max_len):
@@ -12,41 +13,45 @@ class Dataset(Dataset):
     
     def __getitem__(self, idx):
         # Extract words and labels from a sentence
-        words = self.df[idx]['words']
-        labels = self.df[idx]['labels']
+        words = self.df.iloc[idx]['words']
+        labels = self.df.iloc[idx]['labels']
+
+        # Original labels to integers
+        labels = [label_to_id[label] for label in labels]
 
         #tokenIDs
         encoding = self.tokenizer(
             words,
             is_split_into_words=True,
-            return_offsets_mappings=True,
+            return_offsets_mapping=True,
             max_length=self.max_len,
             padding='max_length',
             truncation=True,
             return_tensors='pt'
         )
 
-        labels = self.allign_labels(labels, encoding['offset_mapping'])
+        # Allign labels on the new subword tokens
+        alligned_labels = self.allign_labels(labels, encoding['offset_mapping'])
 
-        encoding.pop('offset_mapping')
+        encoding.pop('offset_mapping') # Dont need it anymore
 
         return {
             'input_ids': encoding['input_ids'].squeeze(),
             'attention_mask': encoding['attention_mask'].squeeze(),
-            'labels': torch.tensor(labels, dtype=torch.long)
+            'labels': torch.tensor(alligned_labels, dtype=torch.long)
         }
-    
-    def allign_labels(self,  labels, offsets):
 
+    def allign_labels(self, labels, offsets):
+
+        offsets = offsets.squeeze(0) # Remove unnecessary dimension
         alligned_labels = []
+        label_idx = 0
 
-        for offset, label in zip(offsets, labels):
-            if offset[0] == 0 and offset[1] != 0:
-                alligned_labels.append(label)
+        for offset in offsets:
+            if offset[0].item() == 0 and offset[1].item() != 0:
+                alligned_labels.append(labels[label_idx])
+                label_idx += 1
             else:
                 alligned_labels.append(-100)
 
         return alligned_labels
-
-
-
