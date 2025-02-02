b2ae99e29007ab74f051e046138064598f2fa240
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-02-02 11:46:49 +0100

    added more metrics logging

--- src/training/train.py
+++ src/training/train.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from tqdm import tqdm
-
 
 def train_model(model, train_dataset, val_dataset, optimizer, batch_size, num_epochs, device):
 
@@ -13,7 +13,11 @@ def train_model(model, train_dataset, val_dataset, optimizer, batch_size, num_ep
 
     history = {
         'train_loss': [],
-        'val_loss': []
+        'val_loss': [],
+        'accuracy': [],
+        'precision': [],
+        'recall': [],
+        'f1_score': []
     }
 
     for epoch in range(num_epochs):
@@ -41,6 +45,8 @@ def train_model(model, train_dataset, val_dataset, optimizer, batch_size, num_ep
         model.eval() ## evaluation mode ##
 
         val_loss = 0
+        all_preds = []
+        all_labels = []
 
         with torch.no_grad():
             for batch in tqdm(val_loader):
@@ -51,16 +57,36 @@ def train_model(model, train_dataset, val_dataset, optimizer, batch_size, num_ep
 
                 outputs = model(inputs, masks, labels)
                 loss = outputs.loss
+                logits = outputs.logits
+
+                preds = torch.argmax(logits, dim=-1)
+                preds = preds.view(-1).cpu().numpy()
+                labels = labels.view(-1).cpu().numpy()
+
+                valid_indices = labels != -100
+                preds = preds[valid_indices]
+                labels = labels[valid_indices]
+
+                all_preds.extend(preds)
+                all_labels.extend(labels)
 
                 val_loss += loss.item()
         
         val_loss /= len(val_loader)
-    
+        accuracy = accuracy_score(all_labels, all_preds)
+        precision, recall, f1, _ = precision_recall_fscore_support(all_labels, all_preds, average='weighted') # TODO: Check if imbalanced
+
         history['train_loss'].append(train_loss)
         history['val_loss'].append(val_loss)
+        history['accuracy'].append(accuracy)
+        history['precision'].append(precision)
+        history['recall'].append(recall)
+        history['f1_score'].append(f1)
 
         print(f'Epoch {epoch+1}/{num_epochs} | '
-              f'Train loss: {train_loss:.4f}, Val loss: {val_loss:.4f}')
+              f'Train loss: {train_loss:.4f}, Val loss: {val_loss:.4f}'
+              f'Acc: {accuracy:.4f}, Prec: {precision:.4f}'
+              f'Rec: {recall:.4f}, F1: {f1:.4f}')
     
     print('Training says SUUIII!')
 
