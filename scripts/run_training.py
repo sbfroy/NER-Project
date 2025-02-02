91ed203db85755608fd70e6e9749c299f86d6a73
Simen Bihaug-Frøyland <simenbih@student.uia.no>
2025-02-02 11:46:10 +0100

    added config to training

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -1,5 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from pathlib import Path
 from transformers import AutoTokenizer
@@ -8,7 +12,7 @@ from src.data.preprocessing import create_df
 from src.utils.config_loader import load_config
 from src.utils.label_mapping import label_to_id
 from src.utils.seed import seed_everything
-from src.models.BERT_model import BERT
+from src.models.transformer_model import TransformerModel
 from src.training.train import train_model
 
 base_dir = Path(__file__).parent.parent
@@ -19,16 +23,16 @@ seed_everything(config['general']['seed'])
 
 tokenizer = AutoTokenizer.from_pretrained(config['model']['model_name'])
 
-model = BERT(model_name=config['model']['model_name'], num_labels=len(label_to_id))
+model = TransformerModel(model_name=config['model']['model_name'], num_labels=len(label_to_id))
 
 # Data loading
 train_df = create_df(base_dir / 'data/norne/ud/nob/no_bokmaal-ud-train.conllu')
 val_df = create_df(base_dir / 'data/norne/ud/nob/no_bokmaal-ud-dev.conllu')
  
-train_dataset = Dataset(train_df, tokenizer, 69)
-val_dataset = Dataset(val_df, tokenizer, 69)
+train_dataset = Dataset(train_df, tokenizer, config['data']['max_seq_len'])
+val_dataset = Dataset(val_df, tokenizer, config['data']['max_seq_len'])
 
-optimizer = optim.AdamW(model.parameters(), lr=3e-5)
+optimizer = optim.AdamW(model.parameters(), lr=config['training']['learning_rate'])
 
 # TODO: Implement my own criterion
 
@@ -41,3 +45,5 @@ history = train_model(
     num_epochs = config['training']['num_epochs'],
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 )
+
+torch.save(model.state_dict(), base_dir / 'src/models/transformer_model.pth')
