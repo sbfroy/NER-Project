b871bf10a1e28b6cb105f1d6d3961c223a5cba98
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-02-21 16:53:40 +0100

    Refactor run_optuna script for regplans dataset; update dropout and learning rate ranges, and modify study parameters for optimization

--- scripts/run_optuna.py
+++ scripts/run_optuna.py
@@ -10,7 +10,7 @@ from transformers import AutoTokenizer
 from src.data.dataset import Dataset
 from src.data.preprocessing import create_df
 from src.utils.config_loader import load_config
-from src.utils.label_mapping import label_to_id
+from src.utils.label_mapping_regplans import label_to_id, id_to_label
 from src.utils.seed import seed_everything
 from src.models.transformer_model import TransformerModel
 from src.training.train import train_model
@@ -22,13 +22,15 @@ config = load_config(base_dir / 'model_params.yaml')
 
 seed_everything(config['general']['seed'])
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 optuna.logging.set_verbosity(optuna.logging.INFO)
 
 tokenizer = AutoTokenizer.from_pretrained(config['model']['model_name'])
 
 # Data loading
-train_df = create_df(base_dir / 'data/norne/ud/nob/no_bokmaal-ud-train.conllu')
-val_df = create_df(base_dir / 'data/norne/ud/nob/no_bokmaal-ud-dev.conllu')
+train_df = create_df(base_dir / 'data/my_data/regplans-train.conllu')
+val_df = create_df(base_dir / 'data/my_data/regplans-dev.conllu')
  
 train_dataset = Dataset(train_df, tokenizer, config['data']['max_seq_len'])
 val_dataset = Dataset(val_df, tokenizer, config['data']['max_seq_len'])
@@ -36,8 +38,8 @@ val_dataset = Dataset(val_df, tokenizer, config['data']['max_seq_len'])
 def objective(trial):
 
     # params to tune
-    dropout = trial.suggest_float('dropout', 0.1, 0.5, step=0.05)
-    learning_rate = trial.suggest_float('learning_rate', 3e-6, 5e-4, log=True)
+    dropout = trial.suggest_float('dropout', 0.1, 0.4, step=0.05)
+    learning_rate = trial.suggest_float('learning_rate', 1e-6, 1e-4, log=True)
     batch_size = trial.suggest_int('batch_size', 8, 32, step=8)
 
     model = TransformerModel(
@@ -48,29 +50,28 @@ def objective(trial):
 
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
 
-    history = train_model(
+    best_f1 = train_model(
         model = model,
         train_dataset = train_dataset,
         val_dataset = val_dataset,
         optimizer = optimizer,
         batch_size = batch_size,
-        num_epochs = config['training']['num_epochs'],
-        device = 'cuda' if torch.cuda.is_available() else 'cpu',
+        num_epochs = config['training']['general']['num_epochs'],
+        device = device,
+        id_to_label = id_to_label,
         trial=trial,
         verbose=False
     )
 
-    best_val_loss = min(history['val_loss'])
-
-    return best_val_loss
+    return best_f1
 
 # Optuna study
 study = optuna.create_study(
-    study_name='transformer_study',
-    direction='minimize', 
+    study_name='NER_Study',
+    direction='maximize', 
     sampler=optuna.samplers.TPESampler(seed=config['general']['seed']),
     load_if_exists=True, 
-    storage='sqlite:///transformer_study.db'
+    storage='sqlite:///NER_Study.db'
 )
 
 study.optimize(objective, n_trials=50, show_progress_bar=True)
