e42e72106ce006df2ebaac8e29d193513da14ebc
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-02-22 16:50:30 +0100

    Add support for setting random seed in transformers library

--- src/utils/seed.py
+++ src/utils/seed.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 import random
+import optuna
+import transformers
 
 def seed_everything(seed):
     torch.manual_seed(seed)
@@ -9,3 +11,5 @@ def seed_everything(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+    transformers.set_seed(seed)
