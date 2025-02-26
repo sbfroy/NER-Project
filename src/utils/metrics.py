756904a31fc4a87a3c7ade4c30a6608b555d6a69
Simen Bihaug-Frøyland <sbfroyland@gmail.com>
2025-02-26 11:53:52 +0100

    Add SpanAcc metric for accurate span extraction and evaluation

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -61,3 +61,52 @@ class SpanF1(Metric): # Custom metric to compute F1-score for entity spans
             'span_f1': f1
         }
     
+class SpanAcc(Metric): # 
+    def __init__(self, id_to_label):
+        super().__init__()
+        self.id_to_label = id_to_label
+        
+        self.add_state('correct_spans', default=torch.tensor(0, dtype=torch.float32), dist_reduce_fx='sum')
+        self.add_state('gold_spans', default=torch.tensor(0, dtype=torch.float32), dist_reduce_fx='sum')
+    
+    def extract_spans(self, labels):
+       
+        spans = set()
+        start, end = None, None
+
+        for idx, label_id in enumerate(labels):
+            label = self.id_to_label[label_id]
+
+            if label == 'B-FELT':
+                if start is not None: spans.add((start, end))
+                start = idx
+                end = idx
+
+            elif label == 'I-FELT' and start is not None:
+                # Continue span
+                end = idx  
+
+            else:
+                if start is not None: spans.add((start, end))  # Save last span
+                start, end = None, None
+
+        if start is not None:
+            spans.add((start, end))
+
+        return spans # Set of (start, end) tuples
+   
+    def update(self, preds: torch.Tensor, targets: torch.Tensor):
+        
+        preds = preds.reshape(1, -1)
+        targets = targets.reshape(1, -1)
+
+        for pred, target in zip(preds, targets):
+            pred_spans = self.extract_spans(pred.tolist())
+            gold_spans = self.extract_spans(target.tolist())
+
+            self.correct_spans += len(pred_spans & gold_spans)
+            self.gold_spans += len(gold_spans)
+
+    def compute(self):
+        return self.correct_spans / self.gold_spans if self.gold_spans > 0 else torch.tensor(0.0)
+    
